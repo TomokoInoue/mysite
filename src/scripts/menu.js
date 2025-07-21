652e5142c8515e8f45c01a4885efe93568c22562
TomokoInoue <tomo.inoue0521@gmail.com>
2025-07-22 00:06:15 +0900

    ヘッダーの微調整

--- src/scripts/menu.js
+++ src/scripts/menu.js
@@ -1,6 +1,21 @@
 'use strict';
 console.log('読み込み');
 
-document.querySelector('.hamburger').addEventListener('click', () => {
-  document.querySelector('.nav-links').classList.toggle('expanded');
-});
+// document.querySelector('.hamburger').addEventListener('click', () => {
+//   document.querySelector('.nav-links').classList.toggle('expanded');
+// });
+
+// 時間の表示
+function updateTime() {
+  const nowUTC = new Date();
+  const offset = 9; // 東京・釜山どちらもUTC+9
+
+  const localTime = new Date(nowUTC.getTime() + nowUTC.getTimezoneOffset() * 60000 + offset * 3600000);
+  const hh = String(localTime.getHours()).padStart(2, '0');
+  const mm = String(localTime.getMinutes()).padStart(2, '0');
+
+  document.getElementById('time').textContent = `${hh}:${mm}`;
+}
+
+updateTime();
+setInterval(updateTime, 1000);
