b5c57a212a3a2ee053586faafb3fe64a385bd797
Tomoko Inoue <tomokoinoue@tomokonoMacBook-Pro.local>
2026-06-19 17:34:27 +0900

    [add]Google Analytics(GA4)を導入

--- src/consts.ts
+++ src/consts.ts
@@ -1,5 +1,5 @@
 // Place any global data in this file.
 // You can import this data from anywhere in your site by using the `import` keyword.
 
-export const SITE_TITLE = 'Tomoko Inoue | 人生珍道中ブログ';
-export const SITE_DESCRIPTION = 'Tomoko Inoue, a graphic designer. I crafting tailored websites with heart and purpose.';
+export const SITE_TITLE = 'Jung Tomoko | 人生珍道中ブログ';
+export const SITE_DESCRIPTION = 'Jung Tomoko, Japanese graphic designer working between Japan and South Korea. Exploring language, culture, and everyday communication through design and this blog.';
